ae43cde628feb7052a2d9f4aaaec835079ad5767
JP Tyndall <jp.tyndall@luckie.com>
2019-11-19 11:05:24 -0500

    init commit

---
+++ gridsome.config.js
@@ -0,0 +1,54 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+const path = require('path')
+
+function addStyleResource(rule) {
+  rule.use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [
+        path.resolve(__dirname, './src/assets/styles/*.scss'),
+      ],
+    })
+}
+
+module.exports = {
+  chainWebpack(config) {
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+
+    types.forEach(type => {
+      addStyleResource(config.module.rule('scss').oneOf(type))
+    })
+  },
+  siteName: 'GB Shop',
+  transformers: {
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      anchorClassName: 'icon icon-link',
+      plugins: []
+    }
+  },
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'posts/**/*.md',
+        typeName: 'Post',
+        remark: {
+          plugins: []
+        }
+      }
+    },
+    {
+      use: 'gridsome-plugin-netlify-cms',
+      options: {
+        publicPath: '/admin'
+      }
+    },
+  ]
+}
---
+++ src/main.js
@@ -0,0 +1,23 @@
+// This is the main.js file. Import global CSS and scripts here.
+// The Client API can be used here. Learn more: gridsome.org/docs/client-api
+
+import Buefy from 'buefy'
+
+import 'buefy/dist/buefy.css'
+
+import '~/assets/styles/main.scss'
+
+import DefaultLayout from '~/layouts/Default.vue'
+
+export default function (Vue, { router, head, isClient }) {
+  head.link.push({
+        rel: 'stylesheet',
+        href: 'https://use.fontawesome.com/releases/v5.2.0/css/all.css'
+  })
+
+  Vue.use(Buefy, {
+    defaultIconPack: 'fas' // Font Awesome Solid
+  })
+  // Set default layout as a global component
+  Vue.component('Layout', DefaultLayout)
+}
